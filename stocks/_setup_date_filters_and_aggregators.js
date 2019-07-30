0ff321205646d30650abd75852b837866ce82019
Valerij Fredriksen <valerij92@gmail.com>
2019-07-30 21:26:46 +0200

    Use simple table instead of dashboard

--- stocks/_setup_date_filters_and_aggregators.js
+++ stocks/_setup_date_filters_and_aggregators.js
@@ -1,42 +1,33 @@
-  var date_range_slider = new google.visualization.ControlWrapper({
-    controlType: 'DateRangeFilter',
-    containerId: 'fake_filter',
-    options: {
-      filterColumnIndex: 0,
-    }
-  });
-
-  var table = new google.visualization.ChartWrapper({
-    chartType: 'Table',
-    containerId: 'value_table_div',
-    options: {
-      allowHtml: true,
-      //showRowNumber: true,
-      width: '100%',
-      sortAscending: false,
-      sortColumn: 0,
-      pageSize: 100,
-    },
-  });
+  var table = new google.visualization.Table(document.getElementById('value_table_div'));
+  var table_options = {
+    allowHtml: true,
+    //showRowNumber: true,
+    width: '100%',
+    sortAscending: false,
+    sortColumn: 0,
+    pageSize: 100,
+  };
 
   var data_table = google.visualization.arrayToDataTable(data);
+  var current_data_table = data_table;
   format_data_table(data_table);
 
-  var dashboard = new google.visualization.Dashboard(document.getElementById('dashboard_div'));
-  dashboard.bind([date_range_slider], [table]);
-  dashboard.draw(data_table);
+  table.draw(data_table, table_options);
 
   var filter = function(event) {
     var chart_extremes = event == null ? chart.xAxis[0].getExtremes() : event;
-    date_range_slider.setState({lowValue: new Date(chart_extremes.min), highValue: new Date(chart_extremes.max)});
-    date_range_slider.draw();
+    var data_view = new google.visualization.DataView(current_data_table);
+    data_view.setRows(current_data_table.getFilteredRows([
+      {column: 0, minValue: new Date(chart_extremes.min), maxValue: new Date(chart_extremes.max)}]));
+
+    table.draw(data_view, table_options);
   }
     
   var aggregator = function() {
     var selected_time_aggregate = document.querySelector('input[name=radio_aggregate_time]:checked').value;
     var selected_number_aggregate = document.querySelector('input[name=radio_aggregate_number]:checked').value;
 
-    var new_data_table = google.visualization.data.group(data_table,
+    current_data_table = google.visualization.data.group(data_table,
       [{column: 0, modifier: getTimeAggregator(selected_time_aggregate), type: 'date'}],
       range(1, width).map(function(i) {
         return {
@@ -46,8 +37,7 @@
         }
       })
     );
-    format_data_table(new_data_table);
-    dashboard.draw(new_data_table);
+    format_data_table(current_data_table);
     filter();
   }
 
