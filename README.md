34c3da6616115d067b2ea6044e1f096e97b653bd
Valerij Fredriksen <valerij92@gmail.com>
2025-04-20 17:59:39 +0200

    Allow filtering multiple types

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # freva.github.io
 
 ```
-podman run -ti --rm -v $(pwd):/srv/jekyll --net host jekyll/jekyll:3 jekyll serve -P 3000
+podman run -ti --rm -v $(pwd):/srv/jekyll --net host docker.io/jekyll/jekyll:3 jekyll serve -P 3000
 ```
--- activities/script.js
+++ activities/script.js
@@ -19,14 +19,29 @@ function createMap(lon, lat, zoom) {
         style: 'mapbox://styles/mapbox/streets-v12',
         center: lat ? [lon, lat ] : [10.420218, 63.437325],
         zoom: zoom ?? 14,
-    })
+    });
+}
+
+function createElement(tag, props = {}, children = []) {
+    const element = document.createElement(tag);
+    Object.entries(props).forEach(([key, value]) => {
+        if (key.startsWith('data-')) {
+            element.setAttribute(key, value);
+        } else {
+            element[key] = value;
+        }
+    });
+    children.forEach(child => element.appendChild(child));
+    return element;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     const searchParams = new URLSearchParams(window.location.search);
     const map = createMap(...(searchParams.get('view')?.split(',')?.map(s => parseFloat(s)) ?? []));
-    const source = searchParams.get('source') ?? 'trimmed_valerij.jsonl';
+    const source = searchParams.get('source');
     const resultsElem = document.getElementById('results');
+
+    if (!source) return;
 
     function updateView() {
         const center = map.getCenter();
@@ -43,6 +58,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 .split('\n')
                 .filter(line => line.length > 0)
                 .map(line => JSON.parse(line)));
+        const allTypes = new Set();
+        activities.forEach(activity => allTypes.add(activity.type));
 
         map.addSource('activities', { type: 'geojson', data: { type: 'FeatureCollection', features: [] }});
         map.addLayer({ id: 'lines', type: 'line', source: 'activities' });
@@ -50,16 +67,13 @@ document.addEventListener('DOMContentLoaded', () => {
         function loadStateFromUrl() {
             const searchParams = new URLSearchParams(window.location.search);
 
-            const type = searchParams.get('type') ?? 'all';
+            const types = new Set(searchParams.get('type')?.split(',') ?? allTypes);
             const from = new Date(searchParams.get('from')).valueOf() / 1000 || 0;
             const to = new Date(searchParams.get('to')).valueOf() / 1000 || Number.MAX_SAFE_INTEGER;
-            const filter = type === 'all'  ? activity => true :
-                           type === 'walk' ? activity => activity.type === 'walk' || activity.type === 'hike' :
-                                             activity => activity.type === type;
 
             let distance = 0, duration = 0, numActivities = 0;
             const features = activities
-                .filter(activity => filter(activity) && activity.start > from && activity.start < to)
+                .filter(activity => types.has(activity.type) && activity.start > from && activity.start < to)
                 .flatMap(activity => {
                     numActivities++;
                     distance += activity.distance;
@@ -77,17 +91,25 @@ document.addEventListener('DOMContentLoaded', () => {
             results.innerHTML = `${numActivities} activities: ${(distance / 1000).toFixed(1)}km in ${toDuration(duration)}`;
         }
 
+        const types = new Set(searchParams.get('type')?.split(',') ?? allTypes);
+        const filterSwitch = document.getElementById('filter-switch');
+        [...allTypes].sort().forEach(type => {
+            const label = createElement('label', { htmlFor: type } );
+            label.innerText = type;
+            filterSwitch.appendChild(createElement('li', { className: 'filter-switch-item' }, [
+                createElement('input', { type: 'checkbox', name: 'type', id: type, value: type, checked: types.has(type) }),
+                label
+            ]));
+        });
+
         document.querySelectorAll('input').forEach(element => {
             element.onchange = event => {
-                updateUrl({ [event.currentTarget.name]: event.currentTarget.checked ? event.currentTarget.id : event.currentTarget.value });
+                const value = event.currentTarget.type === 'checkbox' ?
+                    [...document.getElementsByName(event.currentTarget.name)].filter(e => e.checked).map(e => e.value).sort().join(',') :
+                    event.currentTarget.value;
+                updateUrl({ [event.currentTarget.name]: value });
                 loadStateFromUrl();
             };
-        });
-        searchParams.forEach((value, key) => {
-            let element = document.getElementById(value);
-            if (element) element.checked = true;
-            element = document.getElementById(key);
-            if (element) element.value = value;
         });
 
         loadStateFromUrl();
